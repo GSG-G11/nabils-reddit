dea583c42f12a35894af15652378e7858df58d2d
nabilramy <nabil.abushaban@gmail.com>
2022-03-26 01:53:01 +0300

    made the schema

--- server/database/config/build.sql
+++ server/database/config/build.sql
@@ -1,3 +1,21 @@
 BEGIN;
 
+DROP TABLE IF EXISTS users, posts CASCADE;
+
+create table users (
+  id serial primary key,
+  username varchar(255) not null unique,
+  email varchar(255) not null unique,
+  password varchar(255) not null,
+  created_at timestamp default now()
+);
+
+
+create table posts (
+  id serial primary key,
+  user_id int not null references users(id),
+  title varchar(255) not null,
+  body text not null,
+  created_at timestamp default now()
+);
 COMMIT;
